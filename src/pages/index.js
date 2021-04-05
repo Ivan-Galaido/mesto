a85e6587f2372f7ca8ae17516c904396bd786121
Ivan-Galaido <75268958+Ivan-Galaido@users.noreply.github.com>
2021-04-05 14:42:22 +0300

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,11 @@
 import './index.css';
-import Card from './Card.js';
-import Section from './Section.js';
-import FormValidator from './FormValidator.js';
-import { VALIDATION_SETTINGS } from './FormValidator.js';
-import PopupWithForm from './PopupWithForm.js';
-import PopupWithImage from './PopupWithImage.js';
-import UserInfo from './UserInfo.js';
+import Card from './scripts/Card.js';
+import Section from './scripts/Section.js';
+import FormValidator from './scripts/FormValidator.js';
+import { VALIDATION_SETTINGS } from './scripts/FormValidator.js';
+import PopupWithForm from './scripts/PopupWithForm.js';
+import PopupWithImage from './scripts/PopupWithImage.js';
+import UserInfo from './scripts/UserInfo.js';
 
 export { CardPopup };
 const profileEditButton = document.querySelector('.button_edit');
